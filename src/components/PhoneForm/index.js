3098e68701b7cc3603d21ead0b459d5b39601959
MaksatI2 <zer0icemax@gmail.com>
2025-10-16 17:30:23 +0600

    Added a call button and changed the fonts

---
+++ src/components/PhoneForm/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import Gallery_1 from '../../images/phoneIcon/phone-call-svgrepo-com.svg';
+import './style.css';
+
+const PhoneButton = () => {
+  const handleClick = () => {
+    window.open('https://t.me/Dettroid', '_blank');
+  };
+
+  return (
+    <button 
+      className="phone-button"
+      onClick={handleClick}
+      title="Написать нам"
+      aria-label="Контакт"
+    >
+      <img 
+        src={Gallery_1}
+        alt="phone"
+        className="phone-icon"
+      />
+    </button>
+  );
+};
+
+export default PhoneButton;
