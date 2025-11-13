a8083ec125136b16406fbec382082127729f194a
MaksatI2 <zer0icemax@gmail.com>
2025-11-13 17:17:56 +0600

    fix-6

--- src/components/UpcomingEvents/index.js
+++ src/components/UpcomingEvents/index.js
@@ -1,5 +1,5 @@
 import React, { useState, useRef, useEffect } from 'react';
-import video from '../../images/blog-page/video.webm';
+import video from '../../images/blog-page/video.mp4';
 import './style.css';
 
 const UpcomingEvent = () => {
